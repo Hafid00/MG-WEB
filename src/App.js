904d15ecad5cfd255d57c66a32630d0fd04fb5e9
Hafid00 <hafid.chaikhsidiboubakkar@gmail.com>
2020-01-01 15:33:11 +0100

    integration of: -axios\-react-router-dom -redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Login from './components/Login';
+import store from './store';
+import {Provider} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <a>test</a>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <Provider store={store}>
+      <Router>
+        <div>
+          <Route exact path = "/" component={Login}/>
+        </div>
+      </Router>
+      </Provider>
+      
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div>
+                <a>claro</a>
+            </div>
+        );
+    }
+}
+
+export default Login;
